afd0b77a86685803bdee6c2cec3063327c4bdfad
Helge Sverre <helge.sverre@gmail.com>
2026-01-09 01:57:10 +0100

    not used.

--- src/client/utils/index.ts
+++ src/client/utils/index.ts
@@ -2,14 +2,3 @@ export * from "./formatters.ts";
 export * from "./strings.ts";
 export * from "./markdown.ts";
 export * from "./api.ts";
-
-export function debounce<Args extends unknown[]>(
-  fn: (...args: Args) => void,
-  ms: number,
-): (...args: Args) => void {
-  let timeoutId: ReturnType<typeof setTimeout> | null = null;
-  return (...args: Args) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn(...args), ms);
-  };
-}
